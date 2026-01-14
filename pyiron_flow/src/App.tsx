db086a014dca52ef357290a9d29a79877d0be964
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2026-01-14 15:07:17 +0100

    udate

--- pyiron_flow/src/App.tsx
+++ pyiron_flow/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import {
   ReactFlow,
   addEdge,
@@ -15,6 +15,8 @@ import {
   useReactFlow,
   getOutgoers,
   ReactFlowProvider,
+  ControlButton,
+  Panel,
 } from '@xyflow/react';
 import "./globals.css";
 import './App.css';
@@ -24,32 +26,91 @@ import WorkflowNode from "./components/workflow-node";
 const initialNodes: Node[] = [
   {
     id: '1',
-    data: { label: 'Input Node' },
-    position: { x: 0, y: 0 },
+    data: {
+      "author_name": "John Doe",
+      "author_email": "john.doe@example.com",
+      "node_type": "function",
+      "python_import": "my_nodes.sam.get_speed",
+      "dependencies": null,
+      "source_code": "def get_speed(\n    distance: float,\n    time: float,\n) -> float:\n    speed = distance / time\n    return speed\n",
+      "source_code_hash": "5ad2cfd1830c6c1aa9ce88e008056a099e1970d0954408be9914cc154cd496fa",
+      "docstring": "",
+      "ai_docstring": "",
+      "inputs": {
+        "distance": {
+          "label": null,
+          "datatype": "float",
+          "unit": null,
+          "quantity": null
+        },
+        "time": {
+          "label": null,
+          "datatype": "float",
+          "unit": null,
+          "quantity": null
+        }
+      },
+      "outputs": {
+        "return": {
+          "label": null,
+          "datatype": "float",
+          "unit": null,
+          "quantity": null
+        }
+      }
+    },
+    position: { x: -150, y: 0 },
     type: 'WorkflowNode',
   },
   {
     id: '2',
-    data: { label: 'Process Node' },
-    position: { x: 250, y: 100 },
+    data: {
+      "author_name": "John Doe",
+      "author_email": "john.doe@example.com",
+      "node_type": "function",
+      "python_import": "my_nodes.sam.get_kinetic_energy",
+      "dependencies": null,
+      "source_code": "def get_kinetic_energy(\n    mass: float,\n    velocity: float,\n) -> float:\n    return 0.5 * mass * velocity**2\n",
+      "source_code_hash": "ce29e7286c653c46872d341a68deec1c4e72f82c8b9b45303429457492ca4514",
+      "docstring": "",
+      "ai_docstring": "",
+      "inputs": {
+        "mass": {
+          "label": null,
+          "datatype": "float",
+          "unit": null,
+          "quantity": null
+        },
+        "velocity": {
+          "label": null,
+          "datatype": "float",
+          "unit": null,
+          "quantity": null
+        }
+      },
+      "outputs": {
+        "return": {
+          "label": null,
+          "datatype": "float",
+          "unit": null,
+          "quantity": null
+        }
+      }
+    },
+    position: { x: 150, y: 0 },
     type: 'WorkflowNode',
   },
-  {
-    id: '3',
-    data: { label: 'Output Node' },
-    position: { x: 500, y: 0 },
-    type: 'WorkflowNode',
-  },
+
 ];
 
 const initialEdges: Edge[] = [
-  {
-    id: 'e1-2', source: '1', sourceHandle: 'source-1', target: '2', targetHandle: 'target-1', markerEnd: {
-      type: MarkerType.ArrowClosed, width: 20,
-      height: 20,
-    }
-  },
-  { id: 'e2-3', source: '2', sourceHandle: 'source-1', target: '3', targetHandle: 'target-1', markerEnd: { type: MarkerType.ArrowClosed } },
+  // {
+  //   id: 'e1-2', source: '1', sourceHandle: 'source-1', target: '2', targetHandle: 'target-1', markerEnd: {
+  //     type: MarkerType.ArrowClosed, width: 20,
+  //     height: 20,
+  //   }
+  // },
+  // { id: 'e2-3', source: '2', sourceHandle: 'source-1', target: '3', targetHandle: 'target-1', markerEnd: { type: MarkerType.ArrowClosed } },
 ];
 
 const nodeTypes: NodeTypes = {
@@ -59,6 +120,7 @@ const nodeTypes: NodeTypes = {
 function Flow() {
   const { getNodes, getEdges } = useReactFlow();
 
+  const [rfInstance, setRfInstance] = useState(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
@@ -93,6 +155,13 @@ function Flow() {
     [getNodes, getEdges],
   );
 
+  const onExport = useCallback(() => {
+    if (rfInstance) {
+      const flow = rfInstance.toObject();
+      console.log(JSON.stringify(flow));
+    }
+  }, [rfInstance]);
+
   return (
     <ReactFlow
       nodeTypes={nodeTypes}
@@ -102,11 +171,24 @@ function Flow() {
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
       // isValidConnection={isValidConnection}
+      onInit={setRfInstance}
       fitView
     >
       <Background />
-      <Controls />
+      <Controls>
+        <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
+          A
+        </ControlButton>
+      </Controls>
       <MiniMap />
+      <Panel position="top-right">
+        <button onClick={onExport}>
+          export
+        </button>
+        {/* <button onClick={onImport}>
+          import
+        </button> */}
+      </Panel>
     </ReactFlow>
   );
 }
--- pyiron_flow/src/components/workflow-node.tsx
+++ pyiron_flow/src/components/workflow-node.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
     BaseNode,
     BaseNodeContent,
@@ -6,26 +5,47 @@ import {
     BaseNodeHeader,
     BaseNodeHeaderTitle,
 } from "@/components/base-node";
-import { BaseHandle } from "@/components/base-handle";
+import { LabeledHandle } from "@/components/labeled-handle";
 import { Rocket } from "lucide-react";
 import { memo } from "react";
 import { type NodeProps, Position, Handle, ReactFlowProvider } from "@xyflow/react";
+import { type NodeResponse } from "./NodeResponse"
 
-const WorkflowNode = memo(() => {
+const WorkflowNode = function ({ data }: { data: NodeResponse }) {
     return (
-        <ReactFlowProvider>
-            <BaseNode>
-                <BaseNodeHeader>
-                    <BaseNodeHeaderTitle>Workflow Node</BaseNodeHeaderTitle>
-                </BaseNodeHeader>
-                <BaseNodeContent>
-                    <BaseHandle id="target-1" type="target" position={Position.Left} />
-                    <div>A node with two handles</div>
-                    <BaseHandle id="source-1" type="source" position={Position.Right} />
-                </BaseNodeContent>
-            </BaseNode>
-        </ReactFlowProvider>
+        <BaseNode>
+            <BaseNodeHeader>
+                <BaseNodeHeaderTitle>
+                    {data.python_import ?? <code>source_code_hash: {data.source_code_hash}</code>}
+                </BaseNodeHeaderTitle>
+            </BaseNodeHeader>
+            <BaseNodeContent>
+                <div className="flex flex-row">
+                    <div className="flex-1">
+                        {Object.entries(data.inputs).map(([k, v]) => (
+                            <LabeledHandle
+                                id={`target-${k}`}
+                                title={k}
+                                type="target"
+                                position={Position.Left}
+                                key={k}
+                            />
+                        ))}
+                    </div>
+                    <div className="flex-1 align-right">
+                        {Object.entries(data.outputs).map(([k, v]) => (
+                            <LabeledHandle
+                                id={`source-${k}`}
+                                title={k}
+                                type="source"
+                                position={Position.Right}
+                                key={k}
+                            />
+                        ))}
+                    </div>
+                </div></BaseNodeContent>
+        </BaseNode>
     );
-});
+};
 
 export default WorkflowNode;

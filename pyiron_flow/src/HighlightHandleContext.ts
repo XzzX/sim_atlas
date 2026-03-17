184a03ce908ff91cbb8534969125399fd3e81951
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2026-03-17 10:00:48 +0100

    cleaning up

--- pyiron_flow/src/HighlightHandleContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-import type { FilterState } from './interfaces/FilterState';
-
-export const HighlightHandleContext = createContext<Partial<FilterState> | undefined>(undefined);

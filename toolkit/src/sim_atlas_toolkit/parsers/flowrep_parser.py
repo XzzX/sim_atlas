3465fdee7b6a30b13feacb5c0dd702e97e850834
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2026-06-11 13:09:08 +0200

    some linting

--- toolkit/src/sim_atlas_toolkit/parsers/flowrep_parser.py
+++ toolkit/src/sim_atlas_toolkit/parsers/flowrep_parser.py
@@ -1,46 +1,13 @@
 import inspect
-from typing import Annotated, Any, get_args, get_origin
+from typing import Any
 
-from ..models import Annotation, ArtifactType
+from ..models import ArtifactType
 from .metadata import (
     Metadata,
     enrich_from_docstring,
-    parse_annotation,
     parse_return_annotation,
     parse_signature,
 )
-
-
-def _parse_arguments(sig: inspect.Signature) -> list[Annotation]:
-    arguments: list[Annotation] = []
-    for param_name, param in sig.parameters.items():
-        ann = parse_annotation(param.annotation)
-        ann.label = param_name
-        arguments.append(ann)
-    return arguments
-
-
-def _parse_and_unpack_annotation(annotation: Any) -> list[Annotation]:
-    origin = get_origin(annotation)
-    args = get_args(annotation)
-
-    # unpack one level of Annotated
-    if origin is Annotated:
-        origin = get_origin(args[0])
-        args = get_args(args[0])
-
-    if origin is tuple:
-        annotations: list[Annotation] = []
-        for i, arg in enumerate(args):
-            ann = parse_annotation(arg)
-            ann.label = ann.label if ann.label is not None else str(i)
-            annotations.append(ann)
-        return annotations
-
-    ann = parse_annotation(annotation)
-    if not ann.label:
-        ann.label = "return"
-    return [ann]
 
 
 def parse(obj: Any) -> list[Metadata]:

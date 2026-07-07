ee356ef7757c7266f1c0ddb32d5bb12ffbc59ceb
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2026-07-07 10:09:34 +0000

    extend tests

--- toolkit/tests/mock_api.py
+++ toolkit/tests/mock_api.py
@@ -3,7 +3,7 @@ from typing import Any
 
 import requests
 
-from sim_atlas_toolkit.models import ArtifactRequest
+from sim_atlas_toolkit.models import ArtifactRequest, ExecutionResultRequest
 
 
 class _MockResponse(requests.Response):
@@ -19,7 +19,14 @@ class _MockResponse(requests.Response):
 class NodeStoreAPI:
     def __init__(self) -> None:
         self.uploaded: list[ArtifactRequest] = []
+        self.uploaded_execution_results: list[ExecutionResultRequest] = []
 
     def upload(self, artifacts: list[ArtifactRequest]) -> list[requests.Response]:
         self.uploaded.extend(artifacts)
         return [_MockResponse(str(uuid.uuid4())) for _ in artifacts]
+
+    def upload_execution_result(
+        self, execution_result: ExecutionResultRequest
+    ) -> requests.Response:
+        self.uploaded_execution_results.append(execution_result)
+        return _MockResponse(str(uuid.uuid4()))

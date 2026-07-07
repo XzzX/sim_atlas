77384839a2625ea31c9876bf4111bb7d567e8dcb
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2026-07-07 11:31:31 +0200

    reworked dummy module to include execution results

--- e2e/dummy_module/src/dummy_module/flowrep.py
+++ e2e/dummy_module/src/dummy_module/flowrep.py
@@ -1,13 +1,13 @@
 import flowrep as fr
 
 
-@fr.atomic
+@fr.atomic("sum")
 def add(a: float, b: float) -> float:
     """Returns the sum of a and b."""
     return a + b
 
 
-@fr.atomic
+@fr.atomic("product")
 def mul(a: float, b: float) -> float:
     """Returns the product of a and b."""
     return a * b
@@ -19,3 +19,8 @@ def linear(x: float, slope: float, intercept: float) -> float:
     scaled = mul(x, slope)  # type: ignore
     result = add(scaled, intercept)  # type: ignore
     return result  # type: ignore
+
+run_1 = fr.tools.run_recipe(linear.flowrep_recipe, x=-1.3, slope=-5, intercept=2.1)
+run_2 = fr.tools.run_recipe(linear.flowrep_recipe, x=3.1, slope=2, intercept=0)
+run_3 = fr.tools.run_recipe(linear.flowrep_recipe, x=81.0, slope=9, intercept=-1.1)
+

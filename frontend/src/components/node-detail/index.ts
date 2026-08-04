928d4a1db1bc619865bbde1a8de295d5a9f0b1db
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2026-08-04 10:18:55 +0200

    add knip config

--- frontend/src/components/node-detail/index.ts
+++ frontend/src/components/node-detail/index.ts
@@ -1,4 +1,1 @@
 export { NodeDetailPage } from "./NodeDetailPage";
-export { TypeChip } from "./TypeChip";
-export { ExecutionsTab } from "./ExecutionsTab";
-export { OverviewTab } from "./OverviewTab";
